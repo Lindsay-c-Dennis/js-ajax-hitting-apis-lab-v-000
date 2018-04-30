78623356ffb4e01d95f5de56aaf7a2e77244a250
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-30 17:58:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function getRepositories() {
 }
 
 function displayCommits() {
-  
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = commitsList;
 }
 
 function getCommits(el) {
