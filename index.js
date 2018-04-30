c21b7cc45079d4f4daa2eeebaae7a2a2fb0d7151
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-30 17:20:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let username = document.getElementById("username");
 
+
+
 function getRepositories() {
   const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
   req.open(`https://api.github/users/${username}/repos`);
   req.send();
 }
