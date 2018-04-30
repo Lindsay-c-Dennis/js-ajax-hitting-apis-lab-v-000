3f5c93983ad49157a0d92afdec602824c2a0cad7
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-30 18:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function displayRepositories() {
   let repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>' + repos.map(repos => {
+  const repoList = '<ul>' + repos.map(repos => {
     '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + ' data-username="' + r.owner.login +  '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
 }
