bdda53660e58f32d32a8c8f28b2904c90263d475
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-30 18:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ function getCommits(el) {
 function displayBranches() {
   const branches = JSON.parse(this.responseText);
   const branchesList = `<ul>${branches.map(branch => '<li><h3>' + branch.name + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = branchesList;
 }
 
 function getBranches(el) {
